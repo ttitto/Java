32c75c864f9dc8efb3665193f683f67c00613793
ttitto <ttitto@users.noreply.github.com>
2014-05-15 12:27:13 +0300

    Grid fills the scene size completed

--- SoftUni/JavaFXTestProject/src/GameUIGridTest/GameUIGridTest.java
+++ SoftUni/JavaFXTestProject/src/GameUIGridTest/GameUIGridTest.java
@@ -21,7 +21,10 @@ public class GameUIGridTest extends Application{
 		
 		GridPane grid=new GridPane();
 		grid.setGridLinesVisible(true);
-		//grid.setStyle("-fx-background-color: red");
+		grid.setStyle("-fx-background-color: red; ");
+		//grid.setStyle("-fx-border: 20px solid; -fx-border-color: black;");
+		Scene scene=new Scene(grid, 500, 600);
+		
 		fillGrid(grid, 4);
 		styleGrid(grid);
 		
@@ -29,25 +32,22 @@ public class GameUIGridTest extends Application{
 			System.out.println(n);
 		System.out.println(	grid.getMaxWidth());
 		}
-		Scene scene=new Scene(grid, 500, 600);
+		
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
 	private static void styleGrid(GridPane pane) {
-		pane.setMinSize(30,	30);
+		pane.setStyle("-fx-border: 20px solid; -fx-border-color: black;");
+	pane.setMinSize(pane.getScene().getWidth(), pane.getScene().getHeight());
 		for(Node node:pane.getChildren()){
 			if(node instanceof Control){
 				Control control=(Control) node;
-				control.setMinSize(20,40);
+				control.setMinSize(pane.getScene().getWidth()/4, pane.getScene().getHeight()/4);
 		        control.setStyle("-fx-background-color: cornsilk; -fx-alignment: center;");
 			}
-			if(node instanceof Pane){
-				Pane p=(Pane) node;
-				p.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-				p.setStyle("-fx-background-color: blue; -fx-alignment: center;");
-			}
+			
 		}
 		
 	}
@@ -56,7 +56,10 @@ public class GameUIGridTest extends Application{
 		Label lbl=new Label("test");
 		for (int i = 0; i < squareSize; i++) {
 			pane.addRow(i, new Label("r"+i));
-			pane.addColumn(i, new Label("c"+i));
+			if(3!=i){
+				pane.addColumn(i, new Label("c"+i));
+			}
+			
 			if (3==i) {
 				pane.add(lbl, 0, 2);
 			}
