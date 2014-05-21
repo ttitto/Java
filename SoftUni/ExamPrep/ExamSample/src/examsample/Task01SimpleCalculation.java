e66798434ea4c48b15c317bc09dc070635c634da
ttitto <ttitto@users.noreply.github.com>
2014-05-21 23:18:36 +0300

    Task 01 half solved

---
+++ SoftUni/ExamPrep/ExamSample/src/examsample/Task01SimpleCalculation.java
@@ -0,0 +1,32 @@
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Task01SimpleCalculation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigDecimal x = sc.nextBigDecimal();
+        BigDecimal y = sc.nextBigDecimal();
+        BigDecimal zero = BigDecimal.ZERO;
+
+        if (x == zero && y == zero) {
+            System.out.println("0");
+        } else if (x == zero && y != zero) {
+            System.out.println("5");
+        } else if (y == zero && x != zero) {
+            System.out.println("6");
+        } else if (x.compareTo(zero) > 0 && y.compareTo(zero) > 0) {
+            System.out.println("1");
+        } else if (x.compareTo(zero) < 0 && y.compareTo(zero) > 0) {
+            System.out.println("2");
+        } else if (x.compareTo(zero) < 0 && y.compareTo(zero) < 0) {
+            System.out.println("3");
+        } else if (x.compareTo(zero) > 0 && y.compareTo(zero) < 0) {
+            System.out.println("4");
+        }
+
+    }
+
+}
