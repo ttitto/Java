303ea2b2242bbb6f602b3fbdec9460fe6d30d042
ttitto <ttitto@users.noreply.github.com>
2014-05-27 22:59:47 +0300

    Task 4 solved

--- SoftUni/ExamPrep/ExamSample/src/examsample/Task04MagicCarsNumbers.java
+++ SoftUni/ExamPrep/ExamSample/src/examsample/Task04MagicCarsNumbers.java
@@ -11,88 +11,31 @@ public class Task04MagicCarsNumbers {
         int[] letterValues = {10, 20, 30, 50, 80, 110, 130, 160, 200, 240};
         int magicNum = Integer.parseInt(sc.nextLine());
         int valCount = letterValues.length;
+        StringBuilder sb = new StringBuilder();
+        String patt = "([0-9])([0-9])\\2\\1|([0-9])\\3([0-9])\\4|(0-9){4}|([0-9])([0-9])\\6\\7|([0-9])\\8\\8([0-9])|([0-9])([0-9])\\11\\11";
 
         for (int i = 0; i < valCount; i++) {
             for (int j = 0; j < valCount; j++) {
                 int target = magicNum - 40 - letterValues[i] - letterValues[j];
-                boolean toContinue = true;
+                int current;
                 for (int a = 0; a < 10; a++) {
                     for (int b = 0; b < 10; b++) {
-                        //aaaa
-                        //bbbb
-                        //abbb
-                        //baaa
-                        //aaab
-                        //bbba
-                        //aabb
-                        //bbaa
-                        //abab
-                        //baba
-                        //abba
-                        //baab
-                        
-                        int current = a + b + b + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + a + a + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = a + a + a + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + b + b + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = a + a + b + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + b + a + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = a + b + a + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + a + b + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = a + b + b + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + a + a + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = a + a + a + a;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
-                        }
-                        current = b + b + b + b;
-                        if (increaseCounter(current, target, a, b)) {
-                            continue;
+
+                        for (int c = 0; c < 10; c++) {
+                            for (int d = 0; d < 10; d++) {
+                                current = a + b + c + d;
+                                sb.setLength(0);
+                                sb.append(a).append(b).append(c).append(d);
+                                if (current == target && sb.toString().matches(patt)) {
+                                    resultCounter++;
+                                }
+                            }
                         }
                     }
                 }
             }
         }
         System.out.println(resultCounter);
-    }
-
-    private static boolean increaseCounter(int current, int target, int a, int b) {
-        if (current == target) {
-            resultCounter++;
-            System.out.printf("%s %s %s %s\n", current, target, a, b);
-            return true;
-        }
-        return false;
     }
 
 }
